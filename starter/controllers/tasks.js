b45b6315e3bb0a4874514737b341d1e37b1fa27a
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-10 00:36:11 +0300

    first commit

---
+++ starter/controllers/tasks.js
@@ -0,0 +1,61 @@
+const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
+
+const getAllTasks = asyncWrapper ( async (req,res) =>{
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+})
+
+const createTask = asyncWrapper(async (req,res) =>{
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+})
+
+const getTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findById({ _id: taskID });
+
+    if (!task) {
+        const error = new Error('Not Found');
+        error.status = 404;
+        return next(error);
+    }
+    res.status(200).json({ task }); 
+});
+
+
+
+const deleteTask = asyncWrapper(async (req,res,next) =>{
+    const {id:taskId} = req.params;
+    const task = await Task.findOneAndDelete({_id:taskId});
+
+    if (!task) {
+        const error = new Error('Can not delete task');
+        error.status = 500;
+        return next(error);
+    }
+    res.status(200).json({ task })
+})
+
+const updateTask = asyncWrapper(async (req,res) =>{
+    const {id:taskId} = req.params;
+
+    const task = await Task.findOneAndUpdate({_id:taskId}, req.body, {
+        new: true,
+        runValidators: true,
+    })
+    
+    if (!task) {
+        return res.status(404).json({ msg: `No task with id ${taskId}` });
+    }
+
+    res.status(200).json({ task })
+})
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
---
+++ starter/models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+
+const TaskSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true,'must provide name'],
+        trim:true,
+        maxlength:[20, 'name can not be more than 20']
+    },
+    completed: {
+        type:Boolean,
+        default:false,
+        trim:true
+    }
+})
+
+module.exports = mongoose.model('Task', TaskSchema)
